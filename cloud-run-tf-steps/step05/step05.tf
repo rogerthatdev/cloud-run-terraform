cdd8c791b92dc85123bfa677bf2f7470f10ffab5
Roger Martinez <rogerthatdev@gmail.com>
2022-09-12 15:20:11 -0700

    notes

--- cloud-run-tf-steps/step05/step05.tf
+++ cloud-run-tf-steps/step05/step05.tf
@@ -15,3 +15,18 @@ output "run_last_created_revision" {
 output "run_entire_data_object" {
     value = data.google_cloud_run_service.run_service
 }
+
+
+# Reunning after making a change in step 04 makes no changes. Also, manually deploying a new revision to the new latest does not affect this terraform because it doesn't care about the hash.
+
+
+# Question: what happens if i roll out a revision to something other than 100%?
+
+# Answer: Terraform will break it by changing it back to 100% to latest.
+
+# Bigger question: how is terraform expected to be used for Cloud Run services?  
+## If you config revisions outside TF, it clobbers those changes
+## If you want to manage revisions in TF, you can't unless you have a revision name
+
+
+## Maybe you can use sha instead of "latest"?
