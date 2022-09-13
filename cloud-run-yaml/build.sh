542cae79f2e5ccfc0673a82461e38e8f184b31a7
Roger Martinez <rogerthatdev@gmail.com>
2022-09-12 18:07:57 -0700

    notes

--- cloud-run-yaml/build.sh
+++ cloud-run-yaml/build.sh
@@ -4,3 +4,7 @@ gcloud builds submit \
     --config=cloudbuild.yaml \
     --project="cloud-run-fafo-f241" \
     --substitutions=_REGION="us-central1"
+
+
+# Notes
+# Cloud build service agent needs permissions to AR (Reader), Cloud Run service (developer), and to the service account that will run the Cloud run service (service account user)
