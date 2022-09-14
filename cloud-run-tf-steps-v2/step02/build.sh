e232776a50b335097aebd918f923bf8e7c513e1e
Roger Martinez <rogerthatdev@gmail.com>
2022-09-13 17:34:24 -0700

    step 2 for v2: build and push app to ar

---
+++ cloud-run-tf-steps-v2/step02/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcloud builds submit \
+    --config=web-build.cloudbuild.yaml \
+    --ignore-file=web-build.gcloudignore \
+    --project="cloud-run-fafo-f241" --substitutions=_REGION="us-central1",_SHORT_SHA="latest"
+
+
